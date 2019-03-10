4ab558b473b637a56d9e28db3db4546c3730f297
David Grossman <dgrossman2020@k12.andoverma.us>
2019-03-10 18:36:25 -0400

    UserPanel

---
+++ src/UserPanel.java
@@ -0,0 +1,15 @@
+/*
+* User Panel
+*
+* @author David Grossman
+*
+* @version 2019/3/7
+*/
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class UserPanel extends JPanel implements KeyListener, ActionListener {
+
+}
